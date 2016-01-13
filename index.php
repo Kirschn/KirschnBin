df3320126cfcd43b3f2fcc017e09bfe20a3e116a
Kirschnkiller <kirschnkiller@googlemail.com>
2016-01-13 22:05:50 +0100

    debugging stuff

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ if (isset($_POST["submit"]) && isset($_POST["text"])) {
     // Create mechanic, than redirect to id parameter
     include 'sqlinit.php';
     echo "SQL: INSERT INTO `kirschnbin`.`entrys` (`id`, `text`) VALUES (NULL, ".mysqli_real_escape_string($sqlconnection, $_POST["text"]).");";
-    mysqli_query($sqlconnection, "INSERT INTO `kirschnbin`.`entrys` (`id`, `text`) VALUES (NULL, ".mysqli_real_escape_string($sqlconnection, $_POST["text"]).");");
+    mysqli_query($sqlconnection, "INSERT INTO `kirschnbin`.`entrys` (`id`, `text`) VALUES (NULL, \"".mysqli_real_escape_string($sqlconnection, $_POST["text"])."\");");
     $id=mysqli_insert_id($sqlconnection);
     //header("Location: http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}?id=$id");
 } else if (isset($_GET["id"])) {
     // Output, can be routed in Webserver Config
     include 'sqlinit.php';
-    $text = mysqli_fetch_array(mysqli_query($sqlconnection, "SELECT text FROM entrys WHERE id=".mysqli_real_escape_string($sqlconnection, $_GET["id"]).";"));
+    $text = mysqli_fetch_array(mysqli_query($sqlconnection, "SELECT text FROM entrys WHERE id=\"".mysqli_real_escape_string($sqlconnection, $_GET["id"])."\";"));
     echo var_dump($text);
     die();
 } else {
