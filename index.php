52d9c189ca1bc284fb45c4cef3dfd8b9caf0da36
Kirschnkiller <kirschnkiller@googlemail.com>
2016-01-23 23:01:26 +0100

    Stretched button 100% width

--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 if (isset($_POST["submit"]) && isset($_POST["text"])) {
     include "config.php";
     $sqlconnection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbtable);
-    echo "SQL: INSERT INTO `kirschnbin`.`entrys` (`id`, `text`) VALUES (NULL, ".mysqli_real_escape_string($sqlconnection, $_POST["text"]).");";
-    mysqli_query($sqlconnection, "INSERT INTO `kirschnbin`.`entries` (`id`, `text`) VALUES (NULL, \"".mysqli_real_escape_string($sqlconnection, $_POST["text"])."\");");
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < 6; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    echo "SQL: INSERT INTO `kirschnbin`.`entrys` (`id`, `text`, `uniqid`) VALUES (NULL, ".mysqli_real_escape_string($sqlconnection, $_POST["text"]).");";
+    mysqli_query($sqlconnection, "INSERT INTO `kirschnbin`.`entries` (`id`, `text`) VALUES (NULL, \"".mysqli_real_escape_string($sqlconnection, $_POST["text"])."\", \"" . $randomString . "\");");
     $id=mysqli_insert_id($sqlconnection);
-    header("Location: http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}$id");
+    header("Location: http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}$randomString");
     mysqli_close($sqlconnection);
 } else if (isset($_GET["id"])) {
     include 'config.php';
     $sqlconnection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbtable);
     $loadid = str_replace("/", "", $_GET["id"]);
-    $sqlresult = mysqli_query($sqlconnection, "SELECT text FROM entries WHERE id=\"".mysqli_real_escape_string($sqlconnection, $loadid)."\";");
+    $sqlresult = mysqli_query($sqlconnection, "SELECT text FROM entries WHERE id=\"".mysqli_real_escape_string($sqlconnection, $loadid)."\" OR uniqid=\"".mysqli_real_escape_string($sqlconnection, $loadid)."\";");
     if ($sqlresult !== false) {
     $text = mysqli_fetch_array($sqlresult)[0];
     header("Content-Type: text/plain");
